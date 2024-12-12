a6daf9277899211570e78e4e44c3b1f66be07653
Azzaldeen98 <azzaldeen771211417@gmail.com>
2024-12-12 18:14:07 +0300

    AZM commit 9

---
+++ Infrastructure/Models/Plans/Response/PlansContainerModel.cs
@@ -0,0 +1,15 @@
+﻿using Shared.BaseModels.Plans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Plans.Response
+{
+    public class PlansContainerModel : BasePlansContainer 
+    {
+            public string Id { get; set; }
+        
+    }
+}
