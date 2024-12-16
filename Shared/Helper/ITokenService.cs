605b6c6be33b93c5656e4e66657232269dee407b
Azzaldeen98 <azzaldeen771211417@gmail.com>
2024-12-16 20:04:39 +0300

    AZM commit 18

--- Shared/Helper/ITokenService.cs
+++ Shared/Helper/ITokenService.cs
@@ -3,8 +3,19 @@
     public interface ITokenService
     {
         Task<string?> GetTokenAsync();
+        Task<string> GetTokenTypeAsync();
+        Task<string> GetExpiresInTokenAsync();
+
+        ///////////////////////////////////////
         Task SaveTokenAsync(string token);
+        Task SaveAllTokensAsync(string accessToken, string refreshToken, string expiresInToken, string tokenType);
+        Task SaveTokenTypeAsync(string tokenType);
+        Task SaveExpiresInTokenAsync(string expiresIn);
+        Task SaveRefreshTokenAsync(string token);
+
+        ///////////////////////////////////////
         Task RemoveTokenAsync();
+        Task RemoveAllTokensAsync();
     }
 
 }
